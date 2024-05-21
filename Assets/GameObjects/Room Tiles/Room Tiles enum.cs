3562fa51fbf6c4d64ec0c348a1cb3b12fc073c7b
unknown <bboismenu@gamingcampus.local>
2024-05-21 18:07:23 +0200

    Confused Tileset

---
+++ Assets/GameObjects/Room Tiles/Room Tiles enum.cs
@@ -0,0 +1,17 @@
+// Determines what Zone the Tileset is based on
+public enum ZoneType
+{
+    Debug = -1
+}
+
+
+// Determines which tile prefab is at the location
+public enum TileType
+{
+    Nothing = 0,
+    Flat,
+    SlopeNtS,
+    SlopeStN,
+    SlopeWtE,
+    SlopeEtW
+}
