bbe76204b583cefa8689b3d831109a8603b66fb0
Sam-GTECH <115488661+Sam-GTECH@users.noreply.github.com>
2024-05-23 15:17:00 +0200

    Basic Main Menu

---
+++ Assets/GameObjects/Compenents/Menu/ButtonHandler.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    bool mouse_on = false;
+
+    Vector3 default_scales;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        default_scales = transform.localScale;
+
+        // Changes "Start" text to "Continue" if there's a savefile available
+        if (gameObject.name != "Start") return;
+        TMP_Text text = transform.Find("Text (TMP)").GetComponent<TMP_Text>();
+        if (text is null) return;
+
+        // TODO: Check for savefile
+        if (true)
+        {
+            text.text = "Continue";
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+        if (mouse_on)
+        {
+            float select_mult = GetComponentInParent<MainMenuManager>()._buttonsSelectMult;
+            scale.x = Mathf.MoveTowards(scale.x, default_scales.x * select_mult, 0.15f);
+            scale.y = Mathf.MoveTowards(scale.y, default_scales.y * select_mult, 0.15f);
+            scale.z = Mathf.MoveTowards(scale.z, default_scales.z * select_mult, 0.15f);
+        }
+        else
+        {
+            scale.x = Mathf.MoveTowards(scale.x, default_scales.x, 0.15f);
+            scale.y = Mathf.MoveTowards(scale.y, default_scales.y, 0.15f);
+            scale.z = Mathf.MoveTowards(scale.z, default_scales.z, 0.15f);
+        }
+        transform.localScale = scale;
+    }
+
+    public void MouseEnter()
+    {
+        Debug.Log("Enter");
+        mouse_on = true;
+    }
+
+    public void MouseExit()
+    {
+        Debug.Log("Exit");
+        mouse_on = false;
+    }
+}
--- Assets/GameObjects/Compenents/Menu/MainMenuManager.cs
+++ Assets/GameObjects/Compenents/Menu/MainMenuManager.cs
@@ -1,18 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // By how much buttons will scale up when hovering them
+    public float _buttonsSelectMult = 1.2f;
+
+    public bool _useChildren = false;
+    public List<GameObject> _menuObjects = new();
+
+    readonly Dictionary<string, GameObject> _menus = new();
+    string _currentMenu = string.Empty;
+
+    private void Start()
     {
-        
+        if (_useChildren)
+        {
+            _menuObjects.Clear();
+            foreach (Transform child in transform)
+                _menuObjects.Add(child.gameObject);
+        }
+
+        // Create a dictionary based on the menu_objects list
+        for (int i = 0; i < _menuObjects.Count; i++)
+        {
+            _menus[_menuObjects[i].name] = _menuObjects[i];
+        }
+        BetterDebug.LogDict<string, GameObject>(_menus);
+
+        SetMenu(_menuObjects[0].name);
+        Debug.Log(this.GetMenu("Options"));
+        Debug.Log(this.GetMenu());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Gets the GameObject of the menu name
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public GameObject GetMenu(string name)
     {
-        
+        if (_menus.ContainsKey(name) == false)
+        {
+            throw new KeyNotFoundException("The menu " + name + " does not exist.");
+        }
+        return _menus[name];
+    }
+    /// <summary>
+    /// Gets the GameObject of the current menu
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetMenu()
+    {
+        return _menus[_currentMenu];
+    }
+
+    /// <summary>
+    /// Activates the menu name and deactivate every other one
+    /// </summary>
+    /// <param name="name"></param>
+    /// <exception cref="KeyNotFoundException"></exception>
+    public void SetMenu(string name)
+    {
+        if (_menus.ContainsKey(name) == false)
+        {
+            throw new KeyNotFoundException("The menu "+name+" does not exist.");
+        }
+        foreach (var kvp in _menus)
+        {
+            kvp.Value.SetActive(kvp.Key == name);
+        }
+        _currentMenu = name;
+    }
+
+    /////////////////
+    /// MAIN MENU ///
+    /////////////////
+
+    public void MainMenuStart()
+    {
+        Debug.Log("Start");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void MainMenuOptions()
+    {
+        Debug.Log("Options");
+        SetMenu("Options");
+    }
+
+    public void MainMenuQuit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    ////////////////////
+    /// OPTIONS MENU ///
+    ////////////////////
+
+    public void OptionsMenuBack()
+    {
+        Debug.Log("Options Back Button");
+        SetMenu("Main");
     }
 }
