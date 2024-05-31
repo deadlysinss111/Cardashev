d39dfb9b4ba9dabf1c00e453b638972c4449d7ea
deadlysinss111 <vgautier@gaming.tech>
2024-05-31 17:31:13 +0200

    started some collection stuff

---
+++ Assets/GameObjects/Compenents/Menu/Collection.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    Canvas _canvas;
+
+    List<GameObject> _currentUnlocked;
+    List<GameObject> _currentLocked;
+
+    static Dictionary<Class, List<Card>> _unlocked;
+    static Dictionary<Class, List<Card>> _locked;
+
+    void Load(Class toLoad)
+    {
+        _unlocked = new Dictionary<Class, List<Card>>();
+        _locked = new Dictionary<Class, List<Card>>();
+
+        List<Card> cards;
+
+        _unlocked.TryGetValue(toLoad, out cards);
+        foreach(Card target in cards)
+        {
+            UnityEngine.Object CARD = Resources.Load(target.name);
+            GameObject card = (GameObject)Instantiate(CARD);
+            card.transform.SetParent(_canvas.transform, false);
+            _currentUnlocked.Add(card);
+        }
+
+        _locked.TryGetValue(toLoad, out cards);
+        foreach(Card target in cards)
+        {
+            UnityEngine.Object CARD = Resources.Load(target.name);
+            GameObject card = (GameObject)Instantiate(CARD);
+            card.transform.SetParent(_canvas.transform, false);
+            // Either do something here to distinguish locked card
+            _currentLocked.Add(card);
+        }
+    }
+    
+    // All below values for positions are totally random and meant to be changed later on
+    void Display()
+    {
+        int x = 0;
+        int y = 0;
+        foreach(GameObject target in _currentUnlocked)
+        {
+            target.transform.localPosition = new Vector3(x, y, 0);
+            x += 150;
+            if(x >= 1350)
+            {
+                x = 0;
+                y += 200;
+            }
+        }
+        foreach(GameObject target in _currentLocked)
+        {
+            target.transform.localPosition = new Vector3(x, y, 0);
+            // Either do something here to distinguish locked card
+            x += 150;
+            if (x >= 1350)
+            {
+                x = 0;
+                y += 200;
+            }
+        }
+    }
+}
