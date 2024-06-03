7e5e81b23675d0fddb1bf4d547a3092c728be6bd
Sam-GTECH <115488661+Sam-GTECH@users.noreply.github.com>
2024-06-03 12:31:27 +0200

    Reworked Blocker code

--- Assets/GameObjects/Map/Blocker.cs
+++ Assets/GameObjects/Map/Blocker.cs
@@ -4,38 +4,19 @@ using UnityEngine;
 
 public class Blocker : MonoBehaviour
 {
-    [SerializeField] private float time;
+    [SerializeField] private float _time;
+    [SerializeField] private float _limit;
 
     public GameObject _door1;
     public GameObject _door2;
 
-    private float _startDoor1X;
-    private float _endDoor1X;
-
-    private float _startDoor2X;
-    private float _endDoor2X;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _startDoor1X = _door1.transform.localPosition.x;
-        _endDoor1X = -1.182f;
-
-        _startDoor2X = _door2.transform.localPosition.x;
-        _endDoor2X = 0.394f;
-        BetterDebug.Log(_startDoor1X, _endDoor1X, _startDoor2X, _endDoor2X);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = _door1.transform.localPosition;
-        pos.x = Mathf.Lerp(_startDoor1X, _endDoor1X, Mathf.Clamp01(GlobalInformations._gameTimer/time));
-        _door1.transform.localPosition = pos;
+        Vector3 scale = new(Mathf.Clamp01(GlobalInformations._gameTimer / _time)*_limit, 1, 1);
 
-        pos = _door2.transform.localPosition;
-        pos.x = Mathf.Lerp(_startDoor2X, _endDoor2X, Mathf.Clamp01(GlobalInformations._gameTimer / time));
-        _door2.transform.localPosition = pos;
+        _door1.transform.localScale = scale;
+        _door2.transform.localScale = scale;
     }
 
     private void FixedUpdate()
