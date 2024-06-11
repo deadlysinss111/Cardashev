8b0fe4ed7040caaf4416069a707a983b3597f2b0
Sam-GTECH <115488661+Sam-GTECH@users.noreply.github.com>
2024-06-11 15:11:49 +0200

    Absolutely Horribly Unoptimized RayCasting™

--- Assets/GameObjects/Components/SelectableArea.cs
+++ Assets/GameObjects/Components/SelectableArea.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SelectableArea : MonoBehaviour
 {
-    public List<string> _ignoreLayerList = new() { 
+    [SerializeField] List<string> _ignoreLayerList = new() { 
         "Player",
         "Interactable",
         "Enemy"
@@ -15,6 +15,12 @@ public class SelectableArea : MonoBehaviour
     List<Vector3> _debugRayStart;
     List<Vector3> _debugRayDir;
     List<Color> _debugRayColor;
+
+    [SerializeField] bool _allowSelectEnemy = true;
+    [SerializeField] bool _allowSelectInteractable = false;
+    [SerializeField] bool _allowSelectPlayer = false;
+
+    [SerializeField] int _rayCastMaxDistance = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -39,33 +45,52 @@ public class SelectableArea : MonoBehaviour
 
         ResetDebugRay();
 
+        // This is so unoptimized lmao
         foreach (var tile in _selectableTiles)
         {
             Vector3 pos = tile.transform.position;
-            pos.y += 5;
+            pos.y += 10;
+
+            Vector3 scale = tile.transform.localScale;
+
+            // Ready for 5 raycasts for every tile?
+            Vector3[] posList =
+            {
+                new Vector3(pos.x, pos.y, pos.z),
+                new Vector3(pos.x + scale.x / 4, pos.y, pos.z - scale.z / 4),
+                new Vector3(pos.x - scale.x / 4, pos.y, pos.z + scale.z / 4),
+                new Vector3(pos.x + scale.x / 4, pos.y, pos.z + scale.z / 4),
+                new Vector3(pos.x - scale.x / 4, pos.y, pos.z - scale.z / 4),
+            };
 
             int layerMask = 0;
             foreach (var layer in _ignoreLayerList)
             {
-                if (layer == "Player") continue;
-                layerMask |= 1 << LayerMask.NameToLayer(layer);
+                if (layer == "Player" && _allowSelectPlayer == false) continue;
+                if (layer == "Interactable" && _allowSelectInteractable == false) continue;
+                if (layer == "Enemy" && _allowSelectEnemy == false) continue;
+                layerMask |= (1 << LayerMask.NameToLayer(layer));
             }
 
-            if (Physics.Raycast(pos, Vector3.down, out RaycastHit hit, Mathf.Infinity, layerMask))
+            foreach (var origin in posList)
             {
-                if (hit.transform.gameObject != tile)
+                if (Physics.Raycast(origin, Vector3.down, out RaycastHit hit, _rayCastMaxDistance, layerMask))
                 {
-                    DebugRay(pos, Vector3.down * hit.distance, Color.green);
-                    print($"Detected {hit.transform.gameObject.name} at pos [{pos.x}, {pos.z}]");
+                    if (hit.transform.gameObject != tile)
+                    {
+                        DebugRay(origin, Vector3.down * hit.distance, Color.green);
+                        print($"Detected {hit.transform.gameObject.name} at pos [{pos.x}, {pos.z}]");
+                    }
+                    else
+                    {
+                        DebugRay(origin, Vector3.down * hit.distance, Color.red);
+                    }
                 }
                 else
                 {
-                    DebugRay(pos, Vector3.down * hit.distance, Color.red);
+                    DebugRay(origin, Vector3.down * _rayCastMaxDistance, Color.black);
                 }
-            }
-            else
-            {
-                DebugRay(pos, Vector3.down * 100, Color.black);
+                
             }
         }
     }
@@ -200,6 +225,14 @@ public class SelectableArea : MonoBehaviour
         }
         _selectableTiles.Clear();
     }
+
+    public void SetSelectableEntites(bool allowPlayer = false, bool allowInteractables = false, bool allowEnemies = true)
+    {
+        _allowSelectPlayer = allowPlayer;
+        _allowSelectInteractable = allowInteractables;
+        _allowSelectEnemy = allowEnemies;
+    }
+
     void ResetDebugRay()
     {
         _debugRayStart.Clear();
