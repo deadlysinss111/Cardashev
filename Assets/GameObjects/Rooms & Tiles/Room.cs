af201f556fd22fbab7bf8ac3ac1570a8afeecafe
Baptisto UwU <bboismenu@gaming.tech>
2024-07-02 10:26:22 +0200

    Attempt 1 at enemie lodaing at run-time

--- Assets/GameObjects/Rooms & Tiles/Room.cs
+++ Assets/GameObjects/Rooms & Tiles/Room.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
@@ -38,7 +39,7 @@ public class Room : MonoBehaviour
     */
     public void EnterRoom(/* prefab name comes from GI*/)
     {
-        // Go find and instantiate the room prefab for the floor
+        // Go find the room prefab for the floor
         GameObject roomPrefab = (GameObject)Resources.Load(ROOM_ENCYCLOPEDIA.ZoneFolderName[_zoneType] + " Zone/" + GI._prefabToLoad);
 
         // Prepares some GameObjects we'll need to instantiate quite a few times during this method
@@ -62,22 +63,33 @@ public class Room : MonoBehaviour
                 var newTilemap = Instantiate(tilemapTemplate, tilemap.position, tilemap.rotation, newGrid.transform);
                 newTilemap.name = tilemap.name;
 
-                foreach (Transform model in tilemap.transform)
+                foreach (Transform roomGO in tilemap.transform)
                 {
                     // Finds the correct path to the asset, taking into accounts the Zone Type and asset type
-                    string modelPath = "";
-                    if(tilemap.name == "Interactibles")
-                        modelPath = ROOM_ENCYCLOPEDIA.ZoneFolderName[_zoneType] + " Zone/" + tilemap.name + "/Prefabs/" + model.name;
-                    else
-                        modelPath = ROOM_ENCYCLOPEDIA.ZoneFolderName[_zoneType] + " Zone/" + tilemap.name + "/" + model.name;
+                    string roomGOpath = "";
+                    switch (tilemap.name)
+                    {
+                        case "Interactibles":
+                            roomGOpath = ROOM_ENCYCLOPEDIA.ZoneFolderName[_zoneType] + " Zone/" + tilemap.name + "/Prefabs/" + roomGO.name;
+                            break;
+
+                        case "Population":
+                            roomGOpath = roomGO.name + "/" + roomGO.name;
+                            break;
+
+                        // Topology and Decoration tilemaps
+                        default:
+                            roomGOpath = ROOM_ENCYCLOPEDIA.ZoneFolderName[_zoneType] + " Zone/" + tilemap.name + "/" + roomGO.name;
+                            break;
+                    }
                     //Debug.Log("Attempt to load the prefab " + modelPath);
 
                     // We use the MODEL to get the local position of the object, and the model for the global transform
-                    GameObject MODEL = (GameObject)Resources.Load(modelPath);
+                    GameObject ROOMGO = (GameObject)Resources.Load(roomGOpath);
 
-                    // I cannot understand the reason of it but if we dont offset the MODEL.trnsform by a vector that goes down, the preview become bugged...
-                    Vector3 buf = new Vector3(0, 0, 0) + MODEL.transform.position;
-                    Instantiate(MODEL, model.transform.position + buf , model.transform.rotation, newTilemap.transform);
+                    // I cannot understand the reason of it but if we dont offset the MODEL.transform by a vector that goes down, the preview become bugged...
+                    Vector3 buf = new Vector3(0, 0, 0) + ROOMGO.transform.position;
+                    Instantiate(ROOMGO, roomGO.transform.position + buf , roomGO.transform.rotation, newTilemap.transform);
                 }
             }
             heightLevel++;
