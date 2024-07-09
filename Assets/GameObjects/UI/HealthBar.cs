24ae59a84aeccb5c2416c39b439771bd68be78c4
Sam-GTECH <115488661+Sam-GTECH@users.noreply.github.com>
2024-07-09 16:59:33 +0200

    Temp yellow health bar if Critical

--- Assets/GameObjects/UI/HealthBar.cs
+++ Assets/GameObjects/UI/HealthBar.cs
@@ -11,18 +11,28 @@ public class HealthBar : MonoBehaviour
 
     StatManager _statManager;
 
+    Color _defaultColor;
+
     void Start()
     {
         _healthBar = GameObject.Find("HealthBar");
         GameObject player = GameObject.Find("Player");
         _statManager = player.GetComponent<StatManager>();
         _MaxHealth = _statManager.Health;
+
+        _defaultColor = _healthBar.GetComponent<Slider>().fillRect.gameObject.GetComponent<Image>().color;
     }
 
     void Update()
     {
         if (_statManager != null)
         {
+            // Temp so I can actually see if critical is on or not
+            if (_statManager.HasCritical())
+                _healthBar.GetComponent<Slider>().fillRect.gameObject.GetComponent<Image>().color = Color.yellow;
+            else
+                _healthBar.GetComponent<Slider>().fillRect.gameObject.GetComponent<Image>().color = _defaultColor;
+
             _healthBar.GetComponent<Slider>().value = _statManager.Health / _MaxHealth;
             //_currentHealth = _healthBar.transform.localScale;
             //_currentHealth.x = (_statManager.Health / _MaxHealth);
