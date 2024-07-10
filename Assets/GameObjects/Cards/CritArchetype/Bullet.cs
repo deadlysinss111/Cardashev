e8d9c7cf727dafea2c267fdb88aead351d43c10e
arthus <arthus@meuret.net>
2024-07-10 16:56:34 +0200

    Bullet for skill shots

--- Assets/GameObjects/Cards/CritArchetype/Bullet.cs
+++ Assets/GameObjects/Cards/CritArchetype/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public int _damage;
     Vector3 _direction;
+    float _velocity = 3;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,15 +15,23 @@ public class Bullet : MonoBehaviour
             other.GetComponent<Enemy>().TakeDamage(_damage);
             Destroy(this);
         }
+
+        // TODO : Add a case for obstacle collision
     }
 
     void Update()
     {
-        transform.position += _direction;
+        transform.position += _direction * _velocity * Time.deltaTime;
     }
 
     public void SetDirection(Vector3 direction)
     {
         _direction = direction;
+        transform.rotation = Quaternion.LookRotation(_direction);
+    }
+
+    public void SetInitialPosition(Vector3 position)
+    {
+        transform.position = position;
     }
 }
